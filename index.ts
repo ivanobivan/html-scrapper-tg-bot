fe2bb87c0b5954320ac9199af170b55e97d06653
ivanobivan <ikolesov2009@bk.ru>
2019-04-26 02:01:00 +0300

    scrapper - first step

--- index.ts
+++ index.ts
@@ -1,29 +1,57 @@
 import TelegramBot from "node-telegram-bot-api";
-import config from "./config";
+import path from "path";
+import fs from "fs";
+import https from "https";
+import jsdom from "jsdom";
+
+
+export interface BotConfig {
+    token: string;
+    proxy?: string;
+    strictSSL?: boolean;
+    url: string;
+}
+
+const filePath = path.join(__dirname, "config.json");
+const json = fs.readFileSync(filePath, "utf-8");
+const config:BotConfig = JSON.parse(json);
 
 const scrapperBot = new TelegramBot(config.token, {
     polling: true,
     request: {
+        url: config.url,
         proxy: config.proxy,
         strictSSL: config.strictSSL
     }
 });
 
-scrapperBot.onText(/\/test/, (message) => {
-    scrapperBot.sendMessage(message.chat.id, "Everything is ok");
-});
-
-scrapperBot.onText(/\/get/, (message) => {
-    scrapperBot.sendMessage(message.chat.id, "GET");
-});
-
-scrapperBot.onText(/\/photo/, (message) => {
-    scrapperBot.sendPhoto(message.chat.id, "https://avatars1.githubusercontent.com/u/25548022?s=460&v=4");
+scrapperBot.onText(/\/post/,  async (message) => {
+    const res = await getPostData();
+    scrapperBot.sendMessage(message.chat.id, res);
 });
 
 
-scrapperBot.on("message", (message: TelegramBot.Message) => {
-    const text = message.text;
-    const chatId = message.chat.id;
-    scrapperBot.sendMessage(chatId, `In response: ${text && text.toUpperCase()}`);
-});
+const getPostData = async (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const request = https.request({host: "habr.com", path: "/ru/users/alexzfort/posts/"}, (res) => {
+            let data: string = "";
+
+            res.on("data", (chunk: string) => {
+                data += chunk;
+            });
+
+            res.on("end", () => {
+                const dom = new jsdom.JSDOM(data);
+                const postList = <HTMLUListElement> dom.window.document.querySelector(".content-list");
+                const firstElementChild = <HTMLLIElement> postList.firstElementChild;
+                const postTime = <HTMLSpanElement> firstElementChild.querySelector(".post__time");
+                const link = <HTMLLinkElement> firstElementChild.querySelector(".post__title_link");
+                resolve(`${postTime} ${link}`);
+            });
+        });
+        request.on("error", (e) => {
+            reject(e);
+        });
+        request.end();
+    });
+};
