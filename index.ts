6ff80bdae6f81a5edbde383bdc46b0ed4824bf41
ivanobivan <ikolesov2009@bk.ru>
2019-04-30 00:44:41 +0300

    linter

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ export interface BotConfig {
     proxy?: string;
     strictSSL?: boolean;
     url: string;
-    siteList: Array<{host: string; path: string}>;
+    siteList: Array<{ host: string; path: string }>;
 }
 
 const filePath = path.join(__dirname, "config.json");
@@ -68,7 +68,7 @@ scrapperBot.onText(/\/last/, async message => {
     let article = "";
     if (word) {
         let time;
-        if(months.includes(word[0])) {
+        if (months.includes(word[0])) {
             const index = months.findIndex(e => e.toLowerCase() === word[0]) + 1;
             const trueIndex = index < 10 ? `0${index}` : index.toString();
             const appDate = date
@@ -76,7 +76,7 @@ scrapperBot.onText(/\/last/, async message => {
                 .replace(/[а-яё]+/, "")
                 .replace(/(\d{2}) (\d{2}) (\d{4})  (\d{2}):(\d{2})/, "$3-$2-$1T$4:$5-03:00");
             time = Date.parse(appDate);
-        }else if(serviceWords.includes(word[0])) {
+        } else if (serviceWords.includes(word[0])) {
             const index = serviceWords.find(e => e.toLowerCase() === word[0]);
             if (index === ServiceWords.today) {
                 const tick = date.match(/\d\d:\d\d/);
--- var.ts
+++ var.ts
@@ -1,11 +1,11 @@
-export const months = ['января', 'февраля', 'марта', 'апреля', 'майя', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+export const months = ["января", "февраля", "марта", "апреля", "майя", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 
 export enum ServiceWords {
     today = "сегодня"
 }
 
 export const serviceWords = ["сегодня"];
-export const commands: Array<{ name: string; signature?: string; description: string }> = [
+export const commands: Array<{name: string; signature?: string; description: string}> = [
     {
         name: "post",
         signature: "(number)",
